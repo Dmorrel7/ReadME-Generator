c531cf15306c0b391ba14e9a9d406e86bf88a8a0
Dalton Morrel <daltonmorrel7&@gmail.com>
2020-12-19 12:47:44 -0500

    finished questions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const questionPrompt = () =>
         {
             type: 'input',
             name: 'title',
-            message: 'What is the name of your project?',
+            message: 'What is the name of your project? (Required)',
             validate: nameInput => 
             {
                 if (nameInput)
@@ -30,7 +30,7 @@ const questionPrompt = () =>
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of your project.',
+            message: 'Provide a description of your project. (Required)',
             validate: descriptionInput => 
             {
                 if (descriptionInput)
@@ -47,23 +47,134 @@ const questionPrompt = () =>
 
         {
             type: 'input',
-            name: 'description',
-            message: 'Provide a description of your project.',
-            validate: descriptionInput => 
+            name: 'github',
+            message: 'What is your github username? (Required)',
+            validate: githubInput => 
             {
-                if (descriptionInput)
+                if (githubInput)
                 {
                     return true;
                 }
                 else
                 {
-                    console.log('Please enter a description of your project!');
+                    console.log('Please enter your github username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address? (Required)',
+            validate: emailInput => 
+            {
+                if (emailInput)
+                {
+                    return true;
+                }
+                else
+                {
+                    console.log('Please enter your email address!');
                     return false;
                 }
             }
         },
 
+        {
+            type: 'confirm',
+            name: 'confirmInstallation',
+            message: 'Does your project require instructions for installation?',
+            default: true
+        },
+
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation instructions:',
+            when: ({confirmInstallation}) =>
+            {
+                if (confirmInstallation)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide usage information.',
+            validate: usageInput => 
+            {
+                if (usageInput)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Which license would you like to use for your project?',
+            choices: ['MIT', 'Apache', 'GPL', 'None' ]
+        },
+
+        {
+            type: 'confirm',
+            name: 'confirmTest',
+            message: 'Will your project need instructions for testing?',
+            default: true
+        },
         
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Please provide instructions for testing.',
+            when: ({confirmTest}) =>
+            {
+                if (confirmTest)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'confirm',
+            name: 'confirmContributions',
+            message: 'Will other users be able to contribute to your project?',
+            default: true
+        },
+
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'Please explain guidelines for contribution:',
+            when: ({confirmContributions}) =>
+            {
+                if (confirmContributions)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        },
     ])
 };
 
